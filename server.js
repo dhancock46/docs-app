48b4279ffbed1cdeb066845c412cea2f7b36b8af
dhancock46 <don.r.hancock@gmail.com>
2025-06-22 15:27:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ CHOOSE ONE OF THE FOLLOWING ALTERNATIVES BY CROSSING OUT THE ALTERNATIVE NOT CHO
 
 (A)       **Effective Immediately:** This power of attorney is effective immediately and is not affected by my subsequent disability or incapacity.
 
-~~(B)       ~~~~**Effective Upon Disability or Incapacity:**~~~~ This power of attorney becomes effective upon my disability or incapacity.~~
+~~(B)       Effective Upon Disability or Incapacity: This power of attorney becomes effective upon my disability or incapacity.~~
 
 YOU SHOULD CHOOSE ALTERNATIVE (A) IF THIS POWER OF ATTORNEY IS TO BECOME EFFECTIVE ON THE DATE IT IS EXECUTED.
 
