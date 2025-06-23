e225ae8226eb092176f08cc3e180b7665466dbed
dhancock46 <don.r.hancock@gmail.com>
2025-06-23 14:40:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -217,11 +217,11 @@ async function generateDocument(data) {
           children: [new TextRun({ text: "_______     (M) Tax matters;", font: "Century Gothic" })],
           spacing: { after: 100 }
         }),
-        new Paragraph({
-          children: [new TextRun({ text: "_______     (N) Digital assets and the content of an electronic communication;", font: "Century Gothic" })],
-          spacing: { after: 200 }
-          indent: { hanging: 720 }
-        }),
+       new Paragraph({
+       children: [new TextRun({ text: "_______     (N) Digital assets and the content of an electronic communication;", font: "Century Gothic" })],
+       spacing: { after: 200 },
+       indent: { hanging: 720 }
+       }),
         new Paragraph({
           children: [
             new TextRun({
