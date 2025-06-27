cc7a3679362c777d1143b4d74a4d5d20520e54d7
dhancock46 <don.r.hancock@gmail.com>
2025-06-26 20:13:09 -0500

    Update statutory-generator.js

--- generators/statutory-generator.js
+++ generators/statutory-generator.js
@@ -264,8 +264,8 @@ async function generateStatutoryPOA(data) {
        new Paragraph({
        children: [new TextRun({ text: "________	Authorize another person to exercise the authority granted under this power of attorney", font: "Century Gothic" })],
        spacing: { after: 100 },
-       indent: { left: 720 }
-       }),
+  i    ndent: { hanging: 720 }
+}),
 
         new Paragraph({
           children: [
@@ -294,17 +294,17 @@ async function generateStatutoryPOA(data) {
           spacing: { after: 200 },
           }),
 
-       new Paragraph({
-      children: [new TextRun({ text: "_______	My agent is entitled to reimbursement of reasonable expenses incurred on my behalf and to compensation that is reasonable under the circumstances.", font: "Century Gothic" })],
-      spacing: { after: 100 },
-      indent: { left: 720 }
-      }),
-
      new Paragraph({
-     children: [new TextRun({ text: "_______	My agent is entitled to reimbursement of reasonable expenses incurred on my behalf but shall receive no compensation for serving as my agent.", font: "Century Gothic" })],
-     spacing: { after: 200 },
-     indent: { left: 720 }
-      }),
+  children: [new TextRun({ text: "_______	My agent is entitled to reimbursement of reasonable expenses incurred on my behalf and to compensation that is reasonable under the circumstances.", font: "Century Gothic" })],
+  spacing: { after: 100 },
+  indent: { hanging: 720 }
+}),
+
+new Paragraph({
+  children: [new TextRun({ text: "_______	My agent is entitled to reimbursement of reasonable expenses incurred on my behalf but shall receive no compensation for serving as my agent.", font: "Century Gothic" })],
+  spacing: { after: 200 },
+  indent: { hanging: 720 }
+}),
         new Paragraph({
           children: [
             new TextRun({
